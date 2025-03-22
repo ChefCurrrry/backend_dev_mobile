83f76b14538d61ef97179ac7c1a6c70769d30a51
ChefCurrrry <k.kichenassamy@yahoo.com>
2025-03-22 18:55:17 +0100

    Continuité

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
 import {resolve} from "path";
 import associationRoutes from "./routes/associationRoutes.js";
-import filtrageRoutes from "./routes/filtrageRoutes.js";
 
 dotenv.config({ path: resolve("backend/.env") });
 
@@ -12,11 +11,9 @@ const app = express();
 app.use(express.json()); // Permet d'interpréter les requêtes JSON
 app.use(cors()); // Autorise les requêtes depuis React Native
 
-
+// Routes API pour la gestion des utilisateurs
 app.use("/api/users", userRoutes);
 app.use("/api/associations", associationRoutes);
-app.use("/api/filtrage-associations", filtrageRoutes);
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🚀 Serveur backend démarré sur http://localhost:${PORT}`));
