227fdbdbd451b2be68376b1107bb6c9b8c7e838e
ChefCurrrry <k.kichenassamy@yahoo.com>
2025-03-23 17:25:45 +0100

    Ajout d'une gestion de filtre dans le backend

--- routes/associationRoutes.js
+++ routes/associationRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 // Route pour récupérer toutes les associations
 router.get("/getAsso", async (req, res) => {
     try {
-        const [rows] = await pool.execute("SELECT * FROM ASSOCIATION");
+        const [rows] = await pool.execute("SELECT * FROM ASSOCIATION ORDER BY NomAsso");
         res.json(rows); // Retourne la liste des associations en JSON
     } catch (error) {
         console.error("Erreur lors de la récupération des associations :", error);
